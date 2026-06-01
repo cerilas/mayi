b1f2e4183d60d2b0abf5b9d0962575cda4c310e0
Cerilas <deniz@cerilas.com>
2026-06-01 09:20:29 +0300

    chore: integrate google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr" className="h-full">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KNW9V5F4');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${inter.className} ${montserrat.variable} h-full antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNW9V5F4"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SessionProvider>
           <ThemeProvider>{children}</ThemeProvider>
         </SessionProvider>
